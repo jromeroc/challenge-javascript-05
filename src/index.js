46f463b4d9f1ab6e2f5a921a10a92f77279119cc
jromeroc <jcamiloromeroc@gmail.com>
2020-08-11 20:52:00 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+    let tempArray = [1, 1];
+    let actualArray = [1];
+
+    if(lineNumber === 0) {
+        return actualArray;
+    } 
+
+    if(lineNumber === 1) {
+        return tempArray;
+    }
+
+    for (let i = 2; i <= lineNumber; i++)
+    {
+        actualArray = [1];
+        for(let j = 1; j < i; j++)
+        {
+            actualArray.push(tempArray[j-1] + tempArray[j]);
+        }
+        actualArray.push(1);
+        tempArray = actualArray;
+    }
+    return actualArray;
 }
 
 module.exports = pascalTriangle;
